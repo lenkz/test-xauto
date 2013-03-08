f2f6040f7817f1771f241cc58500abb0f47e7ebd
lenkz <lenkza@gmail.com>
2013-03-08 16:32:05 +0700

    random

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from subprocess import Popen, PIPE
 from time import *
+import random
 #control_f4_sequence = '''keydown Control_L
 #key F4
 #keyup Control_L
@@ -9,6 +10,12 @@ from time import *
 #key A
 #keyup Shift_L
 #'''
+keyball = '''key B
+key a
+key l
+key l
+key Return
+'''
 keytest = '''key N
 key a
 key B
@@ -17,11 +24,12 @@ key L
 key L
 key Return
 '''
+list_call= [keytest,keyball]
 def keypress(sequence):
     p = Popen(['xte'], stdin=PIPE)
     p.communicate(input=sequence)
 while True:
-	keypress(keytest)
-	sleep(1)
+	keypress(random.choice(list_call))
+	sleep(2)
 
 #keypress(control_f4_sequence)
